fed3a50e609147690deea28dc32c67d4bc871d3e
Alvaro F Boirac <alphydan@gmail.com>
2018-01-15 16:20:44 -0400

    minor cleanup of code structure

--- bermycode.py
+++ bermycode.py
@@ -1,6 +1,18 @@
-import time 
+import time
 from display import animation as anim
 
+'''
+This file brings together all the elements of our experiment:
 
+(1) Detect lightnings
+(2) Store data about lightnings [(position, time), ...]
+(3) Save magnetic data [(magnetic field, time)]
+
+'''
+
+
+
+# Show the number of lightnings & a lightning animation on the display
 anim.display_animation(1063)
 
+#

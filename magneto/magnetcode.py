2c951a0ef52857567d8e3f830d8342d5c0bb5614
bermyberries <afeito@bhs.bm>
2018-02-05 04:33:55 +0000

    rounding approximations to save space in magnet data and magnet data sample. Bug was with 'wb' option in opening the file. We want to append each time we run the loop

--- magneto/magnetcode.py
+++ magneto/magnetcode.py
@@ -1,13 +1,11 @@
-import datetime, time
 import math
 import csv
 from sense_hat import SenseHat
 
 se = SenseHat()
-timetaken = 0
 
 '''
-I'm not sure if the above needs to be imported on the main
+Mia: I'm not sure if the above needs to be imported on the main
 file instead of just the current one.
 I've just put it on this file for now
 '''
@@ -19,22 +17,26 @@ def save_magnet_data(timestamp):
   `in`: the input is the timestamp
   `out`: a line is written to file magnetic_data.csv
   '''
-  with open("magnetic_data.csv","wb") as magnetdata:
+
+  # note the use "a" for append
+  # (so we write a new line each time)
+  with open("magneto/magnetic_data.csv","a") as magnetdata:
     writer = csv.writer(magnetdata)
     # gets the raw magnetic data
     raw = se.get_compass_raw()
 
     # gets the components of the magnetic data
-    x = raw["x"]
-    y = raw["y"]
-    z = raw["z"]
+    x = round(raw["x"],5) 
+    y = round(raw["y"],5)
+    z = round(raw["z"],5)
+    # normal fluctuations on the sensor indicate there is no 
+    # sense in saving more than 5 significant figures
     components = "x: {x}, y: {y}, z: {z}".format(**raw)
 
     total = math.sqrt(x*x + y*y + z*z)
-    total = round(total, 3)
+    total = round(total, 6)
   
     row = [str(x),str(y),str(z),str(total), timestamp]
-
     writer.writerow(row)
-    return None # does not output anything
+  return None # does not output anything
 

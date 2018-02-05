e5dfaaf0c1c71588b2b1eb3f7c73b9d52be3794d
bermyberries <afeito@bhs.bm>
2018-02-05 04:34:45 +0000

    upgraded bermycode

--- bermycode.py
+++ bermycode.py
@@ -1,4 +1,5 @@
 import time
+import os # to name files and directories
 
 import datetime as dt
 from picamera import PiCamera
@@ -20,7 +21,7 @@ This file brings together all the elements of our experiment:
 # Parameters & Definitions
 
 # How many seconds does the experiment las?
-x = 1000
+x = 10
 
 # Number of lightnings so far
 n_li = 0
@@ -29,35 +30,38 @@ n_li = 0
 bri_thresh = 40  # number is made up, come back here!!
 
 # Start camera for the whole experiment
-# camera = PiCamera()
-# camera.resolution = (640, 480)
+camera = PiCamera()
+camera.resolution = (640, 480)
+
 
 ############################
 # Hi there astronauts!
-anim.display_greeting()
+# anim.display_greeting() <-- UNCOMMENT LATER
 
 #############################################
 # Main loop
-# looks at Bx,By,Bz and picture every second
+# looks at Bx,By,Bz and picture approx. every second
 
 
 for i in range(x):
     # define timestamp for this loop
     right_now = dt.datetime.now().strftime("%Y_%m_%d-%H_%M_%S_%f")
 
-    mag.save_magnet_data(right_now) # <-- MIA working on this
+    mag.save_magnet_data(right_now) # <-- WORKS!
     print('magnetic data saved')
 
     # capture image for current loop
-    # myimage = camera.capture('/cam/tmp/' + right_now + i + '.jpg')
+    path_to_img = os.path.join(os.getcwd(),'cam/tmp/')
+    image_name = path_to_img + right_now + '-' + str(i) + '.jpg'
+    myimage = camera.capture(image_name)
 
     # check if it's too bright in daylight
-    # briS = brightness_score(myimage) <-- DR FEITO working on this
+    briS = ipro.brightness_score(image_name) # <-- DR FEITO working on this
 
     if briS > bri_thresh:
         # delete image
         pass
-    elif brisS <= bri_thresh:
+    elif briS <= bri_thresh:
         # high_contrast = ipro.high_contrast_image(myimage)
         # <- DR FEITO WORKING on this
         pass
